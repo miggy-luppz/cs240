893c69a3bdb813c3b3fa3cc76993845287cb4bce
miggy-luppz <miguel.lumapat@gmail.com>
2014-04-21 00:38:24 -0400

    Nothing special.

--- Project.cpp
+++ Project.cpp
@@ -42,16 +42,40 @@
 using namespace std;
 const int STOP_WORD_COUNT = 49; // Unless Hartog/TA's trip us over with an entirely new set of stop words, I wc'd the list and got 49.
 
+//EDIT: I just realized that she won't give us the same stop.txt ( and words have mixing cases (lower/upper), so there's that ).
+
+/*
+  ===== make_lower =====
+
+  I feel like there's a string function for this, but I don't care really. 
+  Simply makes the string all lowercase (ignoring intermediary punctuation like hyphens)
+
+*/
+
+void make_lowercase( string& s ) {
+  
+    //Change any uppercase letters to lowercase (all words in stop.txt are lowercase)
+  for( int i = 0; i < s.size(); i++ ) {
+    if( s[i] % 'a' >= 65 ) {
+      s[i] += 32; //Every uppercase letter is 32 away from their lowercase counterpart
+    }
+  }
+
+}
+
 /*
   ===== is_stopWord =====
 
   Made this function to make main() less ugly. Checks if 
   string s is a member of the stopWords list.
  */
-bool is_stopWord( const string& s, const string* stopWords ) {
+bool is_stopWord( string s, const string* stopWords ) {
   
   bool is_sWord = false;
+
+  make_lowercase(s);
   
+  //Compare the word to all of the stop words
   for( int i = 0; i < STOP_WORD_COUNT; i++ ) {
     if( s == stopWords[i] ) {
       is_sWord = true;
@@ -72,13 +96,51 @@ bool is_stopWord( const string& s, const string* stopWords ) {
    but don't forget that there could be 2 occurrences of punctuation on either end.
    So, I just checked twice.
 
+   Pre-condition: s is not a stopWord. 
+
 */
 void strip_punc( string& s ) {
-  if( s[0] % 'A' > 25 && s[0] % 'a' > 25 ) s.erase(0,1);
-  if( s[0] % 'A' > 25 && s[0] % 'a' > 25 ) s.erase(0,1);
-  if( s[s.size() - 1] % 'A' > 25 && s[s.size() - 1] % 'a' > 25 ) s.erase(s.size()-1,1);
-  if( s[s.size() - 1] % 'A' > 25 && s[s.size() - 1] % 'a' > 25 ) s.erase(s.size()-1,1);
+  
+  //Check if the first two characters of the word are punctuation
+  if( s[0] % 'A' > 25 && s[0] % 'a' > 25 ) {
+    s.erase(0,1);
+  }
+  if( s[0] % 'A' > 25 && s[0] % 'a' > 25 ) {
+    s.erase(0,1);
+  }
+  
+  //Check if the last two characters of the word are punctuation
+  if( s[s.size() - 1] % 'A' > 25 && s[s.size() - 1] % 'a' > 25 ) {
+    s.erase(s.size()-1,1);
+  }
+  if( s[s.size() - 1] % 'A' > 25 && s[s.size() - 1] % 'a' > 25 ) {
+    s.erase(s.size()-1,1);
+  }
 }
+
+/* 
+   ===== atoA =====
+   
+   Just changes the first letter of a string s to uppercase if it's
+   a lowercase word or to lowercase if it's uppercase
+   
+   Pre-condition: s has no punctuation at the beginning.
+
+*/
+void atoA( string& s ) {
+
+  if( s[0] % 'A' < 26 ) {
+    s[0]+=32;
+  }
+  
+  //Incase the first letter is a number
+  else if( s[0] % 'a' < 26 ) {
+    s[0]-=32;
+  }
+
+}
+
+
 
 // =========== main =============
 
@@ -96,8 +158,9 @@ int main(int argc,char *argv[]){
   if( stopFile.is_open() ) {
     int stopIter = 0;
     while( getline( stopFile, buffer, '\n' ) ) {
-	stopWords[stopIter] = buffer;
-	stopIter++;
+      make_lowercase(buffer);
+      stopWords[stopIter] = buffer;
+      stopIter++;
     }
   }
   stopFile.close();
@@ -134,22 +197,40 @@ int main(int argc,char *argv[]){
 	    break; //kinda makes the break condition for the for-loop useless, but whatever
 	  }
 	  
-	  //Check if a stopWord or not
-	  if( !(is_stopWord( entry, stopWords )) ) {
-	    
-	    //Strip any punctuation
-	    strip_punc(entry);
+	  //Before anything, strip any punctuation
+	  strip_punc(entry);
+
+	  //Check if a stopWord or not. If it isn't, do the following.
+	  if( !(is_stopWord( entry, stopWords )) ) {	   
 
 	    //If the entry doesn't exist in the map, create a new entry (map a new key)
 	    if( dict.find( entry ) == dict.end() ) {
-	      dict[entry] = 1;
-	      unique++; //Although, I disregarded the number of unique stopWords. Idk if you want that.
+	      
+	      /*Sometimes the entry might be a first occurrence which happened to be a word at
+		at the beginning of a sentence (i.e., what if we're checking 'farm' onto 'Farm').
+		So, this is just another check to make sure that the word is absolutely unique.
+	       */
+
+	      string second_entry = entry; 
+	      atoA( second_entry );
+	      
+	      if( dict.find( second_entry ) == dict.end() ) {
+		dict[entry] = 1;
+		unique++; // I disregarded the number of unique stopWords. But, idk if you want that.
+	      }
+	     
+	      
+	      else {
+		dict[second_entry]++;
+	      }
 	    }
-	    else { //Else, increment the frequency count associated with that existing entry/key.
+	    
+	    //Else, increment the frequency count associated with that existing entry/key.
+	    else { 
 	      dict[entry]+=1;
 	    }
 	  }
-	  //If it is a stopWord, increment stopWord count.
+	  //Else, it is a stopWord. Increment stopWord count.
 	  else {
 	    stop++;
 	  }
@@ -169,12 +250,43 @@ int main(int argc,char *argv[]){
   cout << "Number of unique words: " << unique << endl;
   cout << "Number of words in total: " << total << endl;
 
+  cout << "Percentage of stop words: " << stop * 1.0 / total * 100 << "%" << endl;
+
+
+  // =============== FOR TESTING PURPOSES =====================
+
+  /*string d1 = "Hello";
+  string d2 = "He'llO";
+  string d3 = "He-ll-0";
+  string d4 = "119A44Bgg";
+  string d5 = "ZZZ";
+  string d6 = "'''G'''0";
+  cout << "d1: " << d1 << endl;
+  cout << "d2: " << d2 << endl;
+  cout << "d3: " << d3 << endl;
+  cout << "d4: " << d4 << endl;
+  cout << "d5: " << d5 << endl;
+  cout << "d6: " << d6 << endl;
+  make_lowercase(d1);
+  make_lowercase(d2);
+  make_lowercase(d3);
+  make_lowercase(d4);
+  make_lowercase(d5);
+  make_lowercase(d6);
+  cout << "d1: " << d1 << endl;
+  cout << "d2: " << d2 << endl;
+  cout << "d3: " << d3 << endl;
+  cout << "d4: " << d4 << endl;
+  cout << "d5: " << d5 << endl;
+  cout << "d6: " << d6 << endl;*/
+  
+
   //cout << "Frequency of <TITLE>: " << dict["<TITLE>"] << endl;
   /*for( map<string,int>::iterator mapIter = dict.begin(); mapIter != dict.end(); mapIter++ ) {
     cout << mapIter->first << endl;
     }*/
-  
-  /*string d1 = "Hello,,";
+ 
+   /*string d1 = "Hello,,";
   string d2 = "Hello";
   string d3 = ",\"Hello";
   string d4 = "\"Hello";
